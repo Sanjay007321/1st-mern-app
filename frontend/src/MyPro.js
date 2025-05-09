a6c6dab6d19e4d847e4aeca65c28e32ebdb7646e
Sanjay007321 <saniay007aj@gmail.com>
2025-05-09 23:58:17 +0530

    Update MyPro.js

--- frontend/src/MyPro.js
+++ frontend/src/MyPro.js
@@ -187,7 +187,7 @@ return (
               {
                 bbms.map((item)=> {
                <li key ={item._id} className='list-group-item bg- my-2'>
-                         }
+                         
                <div className='d-flex flex-row gap-5'>
                 {
                   editId !== item._id || editId ==-1 ? (
@@ -209,7 +209,8 @@ return (
                { editId === -1 || editId !== item._id ?<button className='btn btn-danger' onClick={() => handleDelete(item._id)}>Delete</button>:
                <button className='btn btn-danger' onClick={handleCancel}>Cancel</button>}
                </div>
-               </div></li>)
+               </div></li>}
+                 )
               }
             </ul>
             </div>
